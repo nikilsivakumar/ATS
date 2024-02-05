993f2d4bc48bc94ce2e38f876c0c076d78cace1a
Nikil Sivakumar <nikildevikumar@gmail.com>
2024-02-05 16:37:31 +0530

    Updated API

--- app.py
+++ app.py
@@ -1,8 +1,3 @@
-#Field to put JD
-#Upload PDF
-#PDF to image-->Processing-->Google Gemini Pro
-#Prompts Template
-
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -10,25 +5,22 @@ import base64
 import streamlit as st
 import os
 import io
-from PIL import Image
+from PIL import Image 
 import pdf2image
 import google.generativeai as genai
 
-genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
-
-def get_gemini_response(input,pdf_content,prompt):
-    #convert pdf to image
-    #prompt tells what the model needs to be doing
-    model = genai.GenerativeModel('gemini-pro-vision')
-    response = model.generate_content([input,pdf_content[0],prompt])
+#genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+genai.configure(api_key="AIzaSyD9IFVjJtKevhs-LIf1_1kZRGbbCYVEvQU")
+def get_gemini_response(input,pdf_cotent,prompt):
+    model=genai.GenerativeModel('gemini-pro-vision')
+    response=model.generate_content([input,pdf_content[0],prompt])
     return response.text
-
 
 def input_pdf_setup(uploaded_file):
     if uploaded_file is not None:
-        # Convert PDF to image
+        ## Convert the PDF to image
         images=pdf2image.convert_from_bytes(uploaded_file.read())
-        #first page of the image
+
         first_page=images[0]
 
         # Convert to bytes
@@ -45,13 +37,11 @@ def input_pdf_setup(uploaded_file):
         return pdf_parts
     else:
         raise FileNotFoundError("No file uploaded")
-    
 
-# Streamlit App
+## Streamlit App
 
-st.set_page_config(page_title="ATS Resume Expert")
+st.set_page_config(page_title="ATS Resume EXpert")
 st.header("ATS Tracking System")
-#text area so that it can take huge JD
 input_text=st.text_area("Job Description: ",key="input")
 uploaded_file=st.file_uploader("Upload your resume(PDF)...",type=["pdf"])
 
@@ -61,19 +51,19 @@ if uploaded_file is not None:
 
 
 submit1 = st.button("Tell Me About the Resume")
+
 #submit2 = st.button("How Can I Improvise my Skills")
+
 submit3 = st.button("Percentage match")
 
 input_prompt1 = """
- You are an experienced HR with Tech Experience in the field of any one job role from Data Science,
- Full Stack Web development, Big Data Engineering, DEVOPS, Data Analyst,your task is to review the provided resume against the job description. 
+ You are an experienced Technical Human Resource Manager,your task is to review the provided resume against the job description. 
   Please share your professional evaluation on whether the candidate's profile aligns with the role. 
  Highlight the strengths and weaknesses of the applicant in relation to the specified job requirements.
 """
 
 input_prompt3 = """
-You are an skilled ATS (Applicant Tracking System) scanner with a deep understanding of any one job role Data Science,
- Full Stack Web development, Big Data Engineering, DEVOPS, Data Analyst and deep ATS functionality, 
+You are an skilled ATS (Applicant Tracking System) scanner with a deep understanding of data science and ATS functionality, 
 your task is to evaluate the resume against the provided job description. give me the percentage of match if the resume matches
 the job description. First the output should come as percentage and then keywords missing and last final thoughts.
 """
@@ -95,4 +85,3 @@ elif submit3:
         st.write(response)
     else:
         st.write("Please uplaod the resume")
-    
